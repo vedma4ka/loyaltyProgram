8dad59c76aca36f4cf1c77a87c2c0094e70b68d9
IrinaLashitskaya <vedma4ka88@gmail.com>
2021-05-20 11:48:06 +0300

    first

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/redux-store'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( 
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/redux-store.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from "redux";
+
+
+let redusers = combineReducers({
+
+
+});
+
+let store = createStore(redusers);
+
+export default store;
